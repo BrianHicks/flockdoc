feb41695d608035efe59c587f4ed34a79e783f62
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-24 00:12:34 -0500

    add segment_lines function

--- flockdoc/code.py
+++ flockdoc/code.py
@@ -1,16 +1,25 @@
 "functions for handling code"
+from itertools import groupby
 from markdown import markdown
 
 LANGUAGES = {
-    "py": { "single": "#", "multi": ("\"\"\"", "\"\"\"") },
-    "js": { "single": "//", "multi": ("/*", "*/") }
+    "py": "#",
+    "js": "//",
 }
 
 def detect_filetype(filename):
+    'detect a filetype to get comments'
     for ext in reversed(filename.split('.')):
         if ext in LANGUAGES:
             return ext
 
     raise ValueError('Cannot handle "%s". Unknown filetype' % filename)
 
-detect_lines = None
+def segment_lines(source, comment):
+    'segment lines into groups of comment and code'
+    groups = groupby(
+        source.strip().split('\n'),
+        lambda line: 'comment' if line.strip().startswith(comment) else 'code',
+    )
+    for category, group in groups:
+        yield category, '\n'.join(list(group))
--- tests/code_tests.py
+++ tests/code_tests.py
@@ -1,7 +1,8 @@
 "tests for flockdoc/code.py"
-from flockdoc.code import detect_filetype, detect_lines
+from flockdoc.code import detect_filetype, segment_lines
 
 def test_detect_filetype_works():
+    'test detect_filetype'
     cases = {
         "test.py": "py",
         "test.py.md": "py",
@@ -23,3 +24,18 @@ def test_detect_filetype_fails():
         assert False, "detect_filetype failed to fail at detecting 'what.q'"
     except ValueError:
         pass
+
+def test_segment_lines():
+    'test segment_lines'
+    cases = {
+        ("# test\n1", "#"): [('comment', '# test'), ('code', '1')],
+        ("# test\n# test\n1", "#"): [('comment', '# test\n# test'), ('code', '1')],
+        ("# test\n1\n# test", "#"): [('comment', '# test'), ('code', '1'), ('comment', '# test')],
+    }
+    
+    def check(i, o):
+        actual = list(segment_lines(*i))
+        assert actual == o, '%r != %r (got %r)' % (i, o, actual)
+
+    for i, o in cases.items():
+        yield check, i, o
