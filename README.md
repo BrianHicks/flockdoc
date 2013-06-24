458bb1f98a50d66cbf9d8ccf33779c6715440191
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-06-23 22:51:22 -0500

    add project skeleton

---
+++ README.md
@@ -0,0 +1,7 @@
+Flockdoc
+========
+
+Create example API documentation in multiple languages (like [Stripe's API
+docs][stripe-api])
+
+[stripe-api]: http://stripe.com/docs/api 
---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+import flockdoc
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:  # for tox
+        return ''
+
+setup(
+    # System information
+    name='flockdoc',
+    version=flockdoc.__version__,
+    packages=find_packages(exclude=('test',)),
+    scripts=[],
+    zip_safe=True,
+    requires=[
+        
+    ],
+    tests_require=[
+        'nose==1.3.0',
+    ],
+
+    # Human information
+    author='Brian Hicks',
+    author_email='brian@brianthicks.com',
+    url='https://github.com/brianhicks/flockdoc',
+    description='Create example API documentation in multiple languages',
+    long_description=read('README.md'),
+    license='Apache',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
